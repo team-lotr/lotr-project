3fc0c2d33524494a01a41781fea658f169feada0
Felix Gudéhn <felix.gudehn@gmail.com>
2021-02-11 15:47:36 +0100

    Add ring render as initial mockup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import ringRender from "./assets/render.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={ringRender} className="cover" />
+      <div className="content">
+        <h2 className="header">LOTR Project - DH2321</h2>
+      </div>
     </div>
   );
 }
