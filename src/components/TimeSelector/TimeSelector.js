363d8581c0abf045628eb378ce2d5e0723157a35
Felix Gudéhn <felix.gudehn@gmail.com>
2021-02-22 16:37:58 +0100

    Revamp TimeSelector (no slider)

--- src/components/TimeSelector/TimeSelector.js
+++ src/components/TimeSelector/TimeSelector.js
@@ -5,37 +5,44 @@ const width = 360;
 
 export function TimeSelector({ time, range, onChange }) {
   function handleChange(value) {
+    if (!value) {
+      return;
+    }
     onChange(value);
   }
 
-  const maxValue = range[range.length - 1].value;
-  const minValue = range[0].value;
-  const normalizedValues = range.map((date) => date.value / maxValue);
-  const logValues = range.map((date) => Math.log(date.value));
-  const logNormValues = logValues.map((val) => val / logValues[logValues.length - 1]);
-  const minimizedValues = range.map((date) => date.value - minValue);
+  const currentDateIndex = range.findIndex((d) => d.value === time.value);
+  const prevDateIndex = currentDateIndex === 0 ? null : currentDateIndex - 1;
+  const nextDateIndex = currentDateIndex === range.length - 1 ? null : currentDateIndex + 1;
+  const prevDate = range[prevDateIndex];
+  const nextDate = range[nextDateIndex];
 
   return (
     <div className="time-selector">
-      <p>{time ? time.lotrDateString : "Select a date"}</p>
-      <div className="time-selector__picker">
-        {range.map((d) => (
-          <Tick key={d.value} date={d} onClick={onChange} />
-        ))}
-        <div className="time-selector__axis" />
-      </div>
+      <DateDisplay date={range[prevDateIndex]} onClick={() => handleChange(prevDate)} variant="secondary" />
+      <Arrow direction={-1} onClick={prevDate ? () => handleChange(prevDate) : null} />
+      <DateDisplay date={time} onClick={() => {}} variant="primary" />
+      <Arrow direction={1} onClick={nextDate ? () => handleChange(nextDate) : null} />
+      <DateDisplay date={range[nextDateIndex]} onClick={() => handleChange(nextDate)} variant="secondary" />
     </div>
   );
 }
 
-function Tick({ date, onClick }) {
-  const val = (date.year - 3001) * 10 + date.month / 2 + date.day * 5;
+function Arrow({ direction, onClick }) {
+  const symbol = direction > 0 ? ">" : "<";
   return (
-    <div
-      className="time-selector__tick"
-      style={{ transform: `translateX(${val}px` }}
-      data-value={date.lotrDateString}
-      onClick={() => onClick(date)}
-    />
+    <div className="time-selector__arrow" onClick={onClick}>
+      {onClick ? symbol : null}
+    </div>
+  );
+}
+
+function DateDisplay({ date, onClick, variant }) {
+  return (
+    <div className="time-selector__date" data-variant={variant}>
+      <p className="time-selector__datevalue" onClick={onClick}>
+        {date ? date.lotrDateString : null}
+      </p>
+    </div>
   );
 }
