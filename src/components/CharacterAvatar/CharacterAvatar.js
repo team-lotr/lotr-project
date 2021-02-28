4ebb29602565be648b85ca6013b56c820a81dd55
Felix Gudéhn <felix.gudehn@gmail.com>
2021-02-28 13:37:46 +0100

    Add CharacterAvatar component

---
+++ src/components/CharacterAvatar/CharacterAvatar.js
@@ -0,0 +1,9 @@
+import "./character-avatar.scss";
+
+export function CharacterAvatar() {
+  return (
+    <div className="character-avatar">
+      <p>Some Char</p>
+    </div>
+  );
+}
--- src/components/CharacterFilter/CharacterFilter.js
+++ src/components/CharacterFilter/CharacterFilter.js
@@ -1,9 +1,11 @@
 import "./character-filter.scss";
+import { CharacterAvatar } from "../CharacterAvatar";
 
 export function CharacterFilter() {
   return (
     <div className="character-filter">
       <h2 className="character-filter__title">Characters</h2>
+      <CharacterAvatar />
     </div>
   );
 }
