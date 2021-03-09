f110e9f1ec85f6ad34cb6672c33c3f4b7a1e0d28
Felix Gudéhn <felix.gudehn@gmail.com>
2021-03-09 13:35:14 +0100

    Add reduced version of svg map

--- src/components/LotrMap/LotrMap.js
+++ src/components/LotrMap/LotrMap.js
@@ -1,4 +1,5 @@
-import { ReactComponent as MapSvg } from "../../assets/mapome-slim.svg";
+// import { ReactComponent as MapSvg } from "../../assets/mapome-slim.svg";
+import { ReactComponent as MapSvg } from "../../assets/lotr-map-reduced.svg";
 import "./lotr-map.scss";
 
 
