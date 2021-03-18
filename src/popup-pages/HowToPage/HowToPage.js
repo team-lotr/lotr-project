ddf9011b6e01116469053fb264938d4bcb800327
Felix Gudéhn <felix.gudehn@gmail.com>
2021-03-18 22:24:33 +0100

    rickroll :)

--- src/popup-pages/HowToPage/HowToPage.js
+++ src/popup-pages/HowToPage/HowToPage.js
@@ -5,6 +5,15 @@ export function HowToPage({ close }) {
     <div className="how-to-page">
       <h2 className="how-to-page__title">How To</h2>
       <p>This is how you use the app, here is a nice video that you can watch</p>
+      <iframe
+        className="how-to-page__video"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
       <button className="how-to-page__button" onClick={close}>
         Close
       </button>
