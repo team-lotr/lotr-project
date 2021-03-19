7a5f995ab740ee443f58c3ddfc4bf41db803812b
Felix Gudéhn <felix.gudehn@gmail.com>
2021-03-19 11:48:53 +0100

    Add license

--- src/popup-pages/LicensePage/LicensePage.js
+++ src/popup-pages/LicensePage/LicensePage.js
@@ -4,7 +4,16 @@ export function LicensePage({ close }) {
   return (
     <div className="license-page">
       <h2 className="license-page__title">License Information</h2>
-      <p>How others can use the project, we do not claim any right to the material blah blah</p>
+      <a href="https://github.com/team-lotr/lotr-project">Github Repository (Source Code)</a>
+      <p>
+        This project is licensed under the&nbsp;
+        <a href="https://choosealicense.com/licenses/mit/">MIT License</a>, allowing for commercial use, distribution,
+        modification and private use.
+      </p>
+      <p>
+        "There and Back Again" is not associated with Middle-earth Enterprises nor the Tolkien Estates. It is driven by
+        the passion for Tolkiens works.
+      </p>
       <button className="license-page__button" onClick={close}>
         Close
       </button>
