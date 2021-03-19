c960aa0623250c9c5e70fa887056f3a1561cc90a
Felix Gudéhn <felix.gudehn@gmail.com>
2021-03-19 13:40:22 +0100

    Add how to video

--- src/popup-pages/HowToPage/HowToPage.js
+++ src/popup-pages/HowToPage/HowToPage.js
@@ -4,12 +4,15 @@ export function HowToPage({ close }) {
   return (
     <div className="how-to-page">
       <h2 className="how-to-page__title">How To</h2>
-      <p>This is how you use the app, here is a nice video that you can watch</p>
+      <p className="how-to-page__description">
+        Reading instructions can be monotonous, so here is a video that explains all the main interactive elements of
+        the visualisation.
+      </p>
       <iframe
         className="how-to-page__video"
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        // width="560"
+        // height="315"
+        src="https://www.youtube.com/embed/VhQVod8sivg"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
